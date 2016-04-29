f828b265cb5355e3d36428e36b764ab97155becc
NishanthKabra <nishantkabra14@gmail.com>
2016-04-29 14:30:39 +0530

    initial commit

---
+++ push/app/app.ts
@@ -0,0 +1,74 @@
+import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
+import {Push} from 'ionic-native';
+
+@App({
+  templateUrl: 'build/app.html',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+})
+class MyApp {
+  // make HelloIonicPage the root (or first) page
+  rootPage: any = HelloIonicPage;
+  pages: Array<{ title: string, component: any }>;
+
+  constructor(
+    private app: IonicApp,
+    private platform: Platform,
+    private menu: MenuController
+  ) {
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Hello Ionic', component: HelloIonicPage },
+      { title: 'My First List', component: ListPage }
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      var push = Push.init({
+        android: {
+          senderID: "226903902584"
+        },
+        ios: {
+          alert: "true",
+          badge: true,
+          sound: 'false'
+        },
+        windows: {}
+      });
+
+      push.on('registration', (data) => {
+        console.log(data.registrationId);
+      });
+
+      push.on('notification', (data) => {
+        console.log(data.message);
+        console.log(data.title);
+        console.log(data.count);
+        console.log(data.sound);
+        console.log(data.image);
+        console.log(data.additionalData);
+      });
+
+      push.on('error', (e) => {
+        console.log(e.message);
+      });
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    let nav = this.app.getComponent('nav');
+    nav.setRoot(page.component);
+
+  }
+}
